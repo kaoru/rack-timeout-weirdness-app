598ef080297cfbe823501ff76f34096c11e73064
Alex Balhatchet <alex@balhatchet.net>
2020-09-14 16:56:57 +0100

    Configure rack-attack to show request timeout error

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_dispatch.rescue_responses["Rack::Timeout::Error"] = :request_timeout
+  config.action_dispatch.rescue_responses["Rack::Timeout::RequestTimeoutException"] = :request_timeout
+end
--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,1 +1,4 @@
 Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 5
+
+# Do not debug exceptions - we want to test rack-attack behaviour in development
+Rails.application.config.middleware.delete(ActionDispatch::DebugExceptions)
